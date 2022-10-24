0b8b55829cf990749465872f1690a2bc3b1e996f
motzmartin <94078957+motzmartin@users.noreply.github.com>
2022-10-24 17:14:28 +0200

    Add files via upload

--- mvector.c
+++ mvector.c
@@ -1,92 +1,90 @@
 #include "mvector.h"
 
-MVECTOR *mvector_init()
+MVECTOR* mvector_init()
 {
-    MVECTOR *newvector = malloc(sizeof(MVECTOR));
-    if (newvector == NULL)
-    {
-        return NULL;
-    }
-    newvector->first = NULL;
-    newvector->last = NULL;
-    newvector->size = 0;
-    return newvector;
+	MVECTOR* newvector = calloc(1, sizeof(MVECTOR));
+	if (newvector == NULL)
+	{
+		return NULL;
+	}
+	return newvector;
 }
 
-int mvector_push(MVECTOR *vect, void *data)
+int mvector_add(MVECTOR* vect, void* data)
 {
-    MPART *newpart = malloc(sizeof(MPART));
-    if (newpart == NULL)
-    {
-        return 1;
-    }
-    newpart->data = data;
-    newpart->next = NULL;
-    if (vect->first == NULL)
-    {
-        vect->first = newpart;
-    }
-    else
-    {
-        vect->last->next = newpart;
-    }
-    vect->last = newpart;
-    vect->size++;
-    return 0;
+	MPART* newpart = calloc(1, sizeof(MPART));
+	if (newpart == NULL)
+	{
+		return 1;
+	}
+	newpart->data = data;
+	if (vect->first == NULL)
+	{
+		vect->first = newpart;
+	}
+	else
+	{
+		vect->last->next = newpart;
+	}
+	vect->last = newpart;
+	vect->size++;
+	return 0;
 }
 
-void *mvector_get(MVECTOR *vect, int index)
+void* mvector_get(MVECTOR* vect, unsigned int index)
 {
-    // Code refait le 20/10/2022
-    if (index < 0 || index > vect->size - 1)
-    {
-        return NULL;
-    }
-    MPART *p = vect->first;
-    while (index > 0)
-    {
-        p = p->next;
-        index--;
-    }
-    return p->data;
+	if (index > vect->size - 1)
+	{
+		return NULL;
+	}
+	MPART* p = vect->first;
+	while (index > 0)
+	{
+		p = p->next;
+		index--;
+	}
+	return p->data;
 }
 
-int mvector_remove(MVECTOR *vect, int index)
+int mvector_remove(MVECTOR* vect, unsigned int index)
 {
-    MPART *temp;
-    if (index == 0)
-    {
-        temp = vect->first;
-        if (temp == NULL)
-        {
-            return 1;
-        }
-        vect->first = vect->first->next;
-    }
-    else
-    {
-        MPART *p = mvector_get(vect, index - 1);
-        if (p == NULL)
-        {
-            return 1;
-        }
-        temp = p->next;
-        p->next = p->next->next;
-    }
-    free(temp);
-    vect->size--;
-    return 0;
+	MPART* temp;
+	if (index == 0)
+	{
+		temp = vect->first;
+		if (temp == NULL)
+		{
+			return 1;
+		}
+		vect->first = vect->first->next;
+	}
+	else
+	{
+		MPART* p = mvector_get(vect, index - 1);
+		if (p == NULL)
+		{
+			return 1;
+		}
+		temp = p->next;
+		p->next = p->next->next;
+	}
+	free(temp);
+	vect->size--;
+	return 0;
 }
 
-void mvector_free(MVECTOR *vect)
+void mvector_free(MVECTOR* vect, int free_data)
 {
-    MPART *p = vect->first;
-    free(vect);
-    while (p != NULL)
-    {
-        MPART *temp = p->next;
-        free(p->data);
-        free(p);
-        p = temp;
-    }
+	MPART* p = vect->first;
+	free(vect);
+	while (p != NULL)
+	{
+		MPART* temp = p->next;
+		if (free_data)
+		{
+			free(p->data);
+		}
+		free(p);
+		p = temp;
+	}
 }
--- mvector.h
+++ mvector.h
@@ -5,52 +5,34 @@
 
 struct mpart
 {
-    void *data;
-    struct mpart *next;
+	void* data;
+	struct mpart* next;
 };
 
 typedef struct mpart MPART;
 
 struct mvector
 {
-    struct mpart *first;
-    struct mpart *last;
-    int size;
+	struct mpart* first;
+	struct mpart* last;
+	unsigned int size;
 };
 
 typedef struct mvector MVECTOR;
 
-/**
- * initialize the vector
- *
- * \returns the created vector on success or NULL on failure
- */
-MVECTOR *mvector_init();
+/* Initialize the vector. */
+MVECTOR* mvector_init();
 
-/**
- * push a data pointer in the specified vector
- *
- * \returns 0 on success or 1 on failure
- */
-int mvector_push(MVECTOR *vect, void *data);
+/* Add a data pointer in the specified vector. */
+int mvector_add(MVECTOR* vect, void* data);
 
-/**
- * get a data pointer of its vector at the specified index
- *
- * \returns the data pointer on success or NULL on failure
- */
-void *mvector_get(MVECTOR *vect, int index);
+/* Get a data pointer of its vector at the specified index. */
+void* mvector_get(MVECTOR* vect, unsigned int index);
 
-/**
- * remove a part of its vector at the specified index
- *
- * \returns 0 on success or 1 on failure
- */
-int mvector_remove(MVECTOR *vect, int index);
+/* Remove a part of its vector at the specified index. */
+int mvector_remove(MVECTOR* vect, unsigned int index);
 
-/**
- * free the specified vector and all parts associated to it
- */
-void mvector_free(MVECTOR *vect);
+/* Free the specified vector and all parts associated to it if "free_data" is set to 1. */
+void mvector_free(MVECTOR* vect, int free_data);
 
 #endif
